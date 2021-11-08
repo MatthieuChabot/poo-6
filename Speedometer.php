e5b0e1795fce8ba204f92cbf775ca43787ed3f07
MatthieuChabot <89902726+MatthieuChabot@users.noreply.github.com>
2021-11-08 10:16:05 +0100

    Add files via upload

---
+++ Speedometer.php
@@ -0,0 +1,21 @@
+<?php
+
+class Speedometer 
+{
+    public const KM_CONVERSION = 1.6;
+
+    public const MILES_CONVERSION = 0.621371;
+
+    public static function convertMilesToKm($miles) {
+
+        return $miles * self::KM_CONVERSION;
+
+    }
+    public static function convertKmToMiles($km) {
+
+        return $km * self::MILES_CONVERSION;
+
+    }
+}
+
+echo Speedometer::convertKmToMiles(10);
